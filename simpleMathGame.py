8969d119c1046054674f1f1ef79a3081ef8246a9
James Prior <jep200404@columbus.rr.com>
2017-09-27 00:43:05 -0400

    simpleMathGame.py: show_statistics(): Simplify print() by eliminating str() calls.

--- simpleMathGame.py
+++ simpleMathGame.py
@@ -54,10 +54,10 @@ def exit_():
 
 def show_statistics():
     percentage = (total_n_correct / (total_n_correct + total_n_wrong)) * 100
-    print('Total Correct:      ' + str(total_n_correct) + '\n'
-          'Total Wrong:        ' + str(total_n_wrong) + '\n'
-          'Total Extra Points: ' + str(total_n_extra_points) + '\n'
-          'Percentage:         ' + str(percentage))
+    print('Total Correct:      ', total_n_correct, '\n'
+          'Total Wrong:        ', total_n_wrong, '\n'
+          'Total Extra Points: ', total_n_extra_points, '\n'
+          'Percentage:         ', percentage)
 
 
 def get_truth_of_answer(problem, correct_answer):
